6a395e904a107b817559cc8eaf97bdd9856aa9cd
Pawel Cyrta <pawel@cyrta.com>
2017-04-10 08:57:18 +0200

    [python] example of stdin & stdout txt processing program

---
+++ languages/python/main_inout.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+
+# input() reads a string with a line of input, stripping the '\n' (newline) at the end..
+t = int(input())  # read a line with a single integer
+for i in range(1, t + 1):
+      n, m = [int(s) for s in input().split(" ")]  # read a list of integers, 2 in this case
+        print("Case #{}: {} {}".format(i, n + m, n * m))
+          # check out .format's specification for more formatting options
